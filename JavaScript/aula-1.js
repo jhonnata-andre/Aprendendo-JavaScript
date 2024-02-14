38c2d776a80c3c8e866a0e90a7b8435b0c2b8790
Jhonnata André <81209973+jhonnata-andre@users.noreply.github.com>
2024-02-14 00:12:22 -0300

    Create aula-1.js

---
+++ JavaScript/aula-1.js
@@ -0,0 +1,15 @@
+
+// Variáveis declaradas com "var" podem ser atualizadas
+var z = 3;
+
+// Teste do var
+var z = 2;
+
+// Variáveis declaradas com var podem ser atualizadas, enquanto variáveis declaradas com const não podem.
+const y = 2;
+const x = 4;
+
+// Ao fazer isso ele vai da erro!!
+// const x = 2;
+
+console.log( z + y + x)
