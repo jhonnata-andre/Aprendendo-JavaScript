83b8b4abb8bdd0e1d9590ceb46364ac526a3e58e
Jhonnata André <81209973+jhonnata-andre@users.noreply.github.com>
2024-02-14 12:41:01 -0300

    Aula 4

---
+++ JavaScript/aula-4.js
@@ -0,0 +1,10 @@
+
+const Quantidade = 10;
+let idade = 18;
+let JhonnataIdade = 14;
+
+for (JhonnataIdade; JhonnataIdade < idade; JhonnataIdade++){
+    console.log(JhonnataIdade);
+}
+
+console.log('Jhonnata agora tem',JhonnataIdade);
